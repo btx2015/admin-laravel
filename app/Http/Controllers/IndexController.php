3b050bc7ab925ce5af3cb025e3c3586a74569bf5
1461483900@qq.com <jindameinv666>
2018-03-28 23:14:42 +0800

    test validation

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,10 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class IndexController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|numeric|max:5',
+            'body'  => 'required',
+        ]);
+        $errors = $validator->errors()->all();
+        $message = '';
+        if(count($errors)){
+            foreach($errors as $error)
+                $message .= $error;
+            return $message;
+        }
+
         return view('index.index');
     }
 }
