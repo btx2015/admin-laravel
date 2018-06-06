30a46d25bf1b2c94017191dc4a4afbefacc545e6
xu.guanghan <1461483900@qq.com>
2018-06-06 17:47:07 +0800

    sas

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,17 +8,24 @@ use Illuminate\Support\Facades\Validator;
 class IndexController extends Controller
 {
     public function index(Request $request){
-        $validator = Validator::make($request->all(), [
-            'title' => 'required|numeric|max:5',
-            'body'  => 'required',
-        ]);
-        $errors = $validator->errors()->all();
-        $message = '';
-        if(count($errors)){
-            foreach($errors as $error)
-                $message .= $error;
-            return $message;
-        }
+        $time = microtime(true);
+        $requestTime = str_replace(array("."),"",$time);
+        var_dump($requestTime);
+        $requestTime .= substr('00000000000000',0,14-strlen($requestTime));
+        var_dump($requestTime);
+        die;
+//        $validator = Validator::make($request->all(), [
+//            'title' => 'required|numeric|max:5',
+//            'body'  => 'required',
+//        ]);
+//        $errors = $validator->errors()->all();
+//        $message = '';
+//        if(count($errors)){
+//            foreach($errors as $error)
+//                $message .= $error;
+//            return $message;
+//        }
+
 
         return view('index.index');
     }
